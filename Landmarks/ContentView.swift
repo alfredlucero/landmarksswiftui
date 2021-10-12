bc840c7f247a62ad07f6eb9d304b822c4a9283c5
Alfred Lucero <alfred.lucero@ucla.edu>
2021-10-11 20:26:21 -0700

    First SwiftUI project to learn

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Alfred  Lucero on 10/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Alfred!")
+            .font(.title)
+            .foregroundColor(Color(hue: 0.449, saturation: 0.529, brightness: 0.668))
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Alfred  Lucero on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct LandmarksApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
