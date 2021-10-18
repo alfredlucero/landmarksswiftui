d5a6dc96e4b96bb751dd7069ee40c0ef9b5f33ad
Alfred Lucero <alfred.lucero@ucla.edu>
2021-10-18 14:09:50 -0700

    Finished adding watchOS app version

---
+++ WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Alfred  Lucero on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
