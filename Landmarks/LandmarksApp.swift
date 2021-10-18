56fd90c28b245c598bf976dd5710a71acf23eeda
Alfred Lucero <alfred.lucero@ucla.edu>
2021-10-18 15:59:47 -0700

    Created MacOS app implementation

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -12,19 +12,28 @@ struct LandmarksApp: App {
     @StateObject private var modelData = ModelData()
     
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environmentObject(modelData)
+        let mainWindow = WindowGroup {
+           ContentView()
+               .environmentObject(modelData)
         }
+        
+        #if os(macOS)
+            mainWindow
+                .commands {
+                    LandmarkCommands()
+                }
+        #else
+            mainWindow
+        #endif
         
         #if os(watchOS)
             WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
         #endif
-    }
-}
-
-struct LandmarksApp_Previews: PreviewProvider {
-    static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+        
+        #if os(macOS)
+           Settings {
+               LandmarkSettings()
+           }
+       #endif
     }
 }
---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Alfred  Lucero on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
